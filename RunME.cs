d1ca5ce295b865c374562b1555c44735f6252f43
Doggo <157554405+WyattSteinmann@users.noreply.github.com>
2025-01-12 17:27:01 -0600

    Delete RunME.cs

--- RunME.cs
+++
@@ -1,2 +0,0 @@
-// I Am Not Using C# Yet... But I Plan To IDK When But I Plan
-Console.WriteLine("I Am Not Using C# Yet... But I Plan To IDK When But I Plan TBH I Wanted To Write Somthing In C# :D");
